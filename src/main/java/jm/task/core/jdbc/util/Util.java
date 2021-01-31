de109f1ac0d5b283a3ad2805eb766871a14dade6
Nikaria <cogat@mail.ru>
2021-01-31 21:10:08 +0300

    2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,35 +1,50 @@
 package jm.task.core.jdbc.util;
 
 import com.mysql.cj.jdbc.Driver;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.metamodel.MetadataSources;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?autoReconnect=true&useSSL=false&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
 
-    public static Connection getConnection() {
-        try {
-            Driver driver = new Driver();
-            DriverManager.registerDriver(driver);
-        } catch (SQLException e) {
-            System.err.println("Неудалось загрузить класс драйвера");
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+                Map<String, String> settings = new HashMap<>();
+                settings.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                settings.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydbtest");
+                settings.put("hibernate.connection.username", "root");
+                settings.put("hibernate.connection.password", "root");
+                settings.put("hibernate.show_sql", "true");
+                settings.put("hibernate.hbm2ddl.auto", "update");
+
+                registryBuilder.applySettings(settings);
+                registry = registryBuilder.build();
+                MetadataSources sources = new MetadataSources(registry)
+                        .addAnnotatedClass(User.class);
+
+                sessionFactory = sources.buildMetadata().buildSessionFactory();
+
+            } catch (Exception e) {
+                System.out.println("SessionFactory creation failed");
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
         }
-
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return null;
-    }
-
-    public static Statement getStatement() throws SQLException {
-        Connection connection = getConnection();
-        return connection.createStatement();
+        return sessionFactory;
     }
 }
